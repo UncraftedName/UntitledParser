ee3a58343aca90c27422cf2e14160c980c448c90
UncraftedName <uncraftedname@gmail.com>
2020-03-07 12:19:55 -0800

    improved ParserUtils.cs for filtering and added some docs

--- DemoParser/src/Utils/ParserUtils.cs
+++ DemoParser/src/Utils/ParserUtils.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using DemoParser.Parser;
+using DemoParser.Parser.Components;
 using DemoParser.Parser.Components.Abstract;
 using DemoParser.Parser.Components.Packets;
 
 namespace DemoParser.Utils {
 	
 	public static class ParserUtils {
-
+		
 		public static IEnumerable<T> FilterForPacketType<T>(this SourceDemo demo) where T : DemoPacket {
 			return demo.Frames
 				.Select(frame => frame.Packet)
@@ -18,30 +19,69 @@ namespace DemoParser.Utils {
 		}
 
 
+		/// <summary>
+		/// Filters for a specific kind of message from a Packet packet.
+		/// </summary>
+		/// <param name="packet"></param>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
 		public static IEnumerable<T> FilterForMessageType<T>(this Packet packet) where T : DemoMessage {
 			return packet
 				.MessageStream
-				.Messages
 				.Select(tuple => tuple.Item2)
 				.Where(message => message != null && message.GetType() == typeof(T))
 				.Cast<T>();
 		}
 
 
-		public static IEnumerable<T> FilterForMessageType<T>(this SourceDemo demo) where T : DemoMessage {
-			return demo.FilterForPacketType<Packet>()
-				.SelectMany(packet => packet.MessageStream.Messages)
+		/// <summary>
+		/// Filters for a specific type of message from a SignOn packet.
+		/// </summary>
+		/// <param name="signOn"></param>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		public static IEnumerable<T> FilterForMessageType<T>(this SignOn signOn) where T : DemoMessage {
+			return signOn
+				.MessageStream
 				.Select(tuple => tuple.Item2)
 				.Where(message => message != null && message.GetType() == typeof(T))
 				.Cast<T>();
 		}
 
 
-		public static IEnumerable<(int tick, T message)> FilterForMessageTypeWithTicks<T>(this SourceDemo demo)
-			where T : DemoMessage // I barely understand this select many linq stuff - rider just did everything for me
-		{
-			return demo.FilterForPacketType<Packet>()
-				.SelectMany(FilterForMessageType<T>, (packet, message) => (packet.Tick, message));
+		/// <summary>
+		/// Gets all message in the demo stored in the Packet packet or the SignOn packet,
+		/// as well as the tick on which each message occured on.
+		/// </summary>
+		/// <param name="demo"></param>
+		/// <returns></returns>
+		public static IEnumerable<(MessageType messageType, DemoMessage message, int tick)> FilterForMessages(this SourceDemo demo) {
+			foreach (DemoPacket packet in demo.Frames.Select(frame => frame.Packet)) {
+				MessageStream m;
+				Type t = packet.GetType();
+				if (t == typeof(SignOn))
+					m = ((SignOn)packet).MessageStream;
+				else if (t == typeof(Packet))
+					m = ((Packet)packet).MessageStream;
+				else
+					continue;
+				foreach ((MessageType messageType, DemoMessage demoMessage) in m)
+					yield return (messageType, demoMessage, packet.Tick);
+			}
+		}
+
+
+		public static IEnumerable<(T message, int tick)> FilterForMessageType<T>(this SourceDemo demo) where T : DemoMessage {
+			return FilterForMessages(demo)
+				.Where(tuple => tuple.message != null && tuple.message.GetType() == typeof(T))
+				.Select(tuple => ((T)tuple.message, tuple.tick));
+		}
+
+
+		public static IEnumerable<(DemoMessage message, int tick)> FilterForMessageType(this SourceDemo demo, MessageType messageType) {
+			return FilterForMessages(demo)
+				.Where(tuple => tuple.message != null && tuple.messageType == messageType)
+				.Select(tuple => (tuple.message, tuple.tick));
 		}
 
 
@@ -53,10 +93,9 @@ namespace DemoParser.Utils {
 		// https://stackoverflow.com/questions/489258/linqs-distinct-on-a-particular-property
 		public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector) {
 			HashSet<TKey> seenKeys = new HashSet<TKey>();
-			foreach (T element in source) {
+			foreach (T element in source)
 				if (seenKeys.Add(keySelector(element)))
 					yield return element;
-			}
 		}
 		
 		
