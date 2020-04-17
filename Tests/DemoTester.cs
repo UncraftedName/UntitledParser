b2c9bc35dab6c7326abca2c8c4236e7dc5b1acb1
UncraftedName <uncraftedname@gmail.com>
2020-04-17 11:35:25 -0700

    added some basic tests for different types of demos

---
+++ Tests/DemoTester.cs
@@ -0,0 +1,74 @@
+using System;
+using System.IO;
+using DemoParser.Parser;
+using NUnit.Framework;
+
+namespace Tests {
+    
+    // i'm not sure how to set this up properly don't judge me
+    public class DemoTester {
+
+        private static readonly string ProjectDir = 
+            Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+        
+
+        private static void ParseDemo(string path) {
+            try {
+                SourceDemo demo = new SourceDemo(path);
+                demo.Parse();
+            } catch (Exception e) {
+                Assert.Fail($"{path} failed to parse: {e.Message}");
+            }
+        }
+        
+
+        [Test]
+        public void Portal1Leak() {
+            ParseDemo($"{ProjectDir}/sample demos/portal 1 leak.dem");
+        }
+        
+        
+        [Test]
+        public void Portal1Unpack() {
+            ParseDemo($"{ProjectDir}/sample demos/portal 1 unpack.dem");
+        }
+        
+        
+        [Test]
+        public void Portal1Steampipe() {
+            ParseDemo($"{ProjectDir}/sample demos/portal 1 steampipe.dem");
+        }
+        
+        
+        [Test]
+        public void Portal13420() {
+            ParseDemo($"{ProjectDir}/sample demos/portal 1 3420.dem");
+        }
+        
+        
+        [Test]
+        public void Portal2Coop() {
+            ParseDemo($"{ProjectDir}/sample demos/portal 2 coop.dem");
+            ParseDemo($"{ProjectDir}/sample demos/portal 2 coop long.dem");
+        }
+        
+        
+        [Test]
+        public void Portal2SinglePlayer() {
+            ParseDemo($"{ProjectDir}/sample demos/portal 2 sp.dem");
+        }
+        
+        
+        [Test]
+        public void L4D2_2000() {
+            ParseDemo($"{ProjectDir}/sample demos/l4d2 2000.dem");
+        }
+        
+        
+        [Test]
+        public void L4D2_2042() {
+            ParseDemo($"{ProjectDir}/sample demos/l4d2 2000.dem");
+            ParseDemo($"{ProjectDir}/sample demos/l4d2 2000_2.dem");
+        }
+    }
+}
