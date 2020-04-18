0304acb17858b6f8bb6c9be3c87215bb2aa31d6b
UncraftedName <uncraftedname@gmail.com>
2020-04-17 18:20:31 -0700

    added bit coord test

--- Tests/BitStreamTester.cs
+++ Tests/BitStreamTester.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
-using NUnit.Framework;
 using DemoParser.Utils.BitStreams;
+using NUnit.Framework;
 
 namespace Tests {
 	
@@ -23,15 +23,14 @@ namespace Tests {
 		[Test]
 		public void WriteBools() {
 			List<bool> bools = new List<bool>();
-			for (int i = 0; i < Count; i++)  {
+			for (int _ = 0; _ < Count; _++)  {
 				bool r = _random.NextDouble() >= .5;
 				bools.Add(r);
 				_writer.WriteBool(r);
 			}
 			_reader = new BitStreamReader(_writer.AsArray);
-			for (int i = 0; i < Count; i++) {
+			for (int i = 0; i < Count; i++) 
 				Assert.AreEqual(bools[i], _reader.ReadBool());
-			}
 			Assert.Pass();
 		}
 		
@@ -40,16 +39,15 @@ namespace Tests {
 		public void WriteUBits() {
 			List<(uint val, int size)> bits = new List<(uint val, int size)>();
 			
-			for (int i = 0; i < Count; i++)  {
-				var r = ((uint)_random.Next(), _random.Next(1, 33));
-				r.Item1 &= uint.MaxValue >> (32 - r.Item2);
+			for (int _ = 0; _ < Count; _++)  {
+				(uint val, int size) r = ((uint)_random.Next(), _random.Next(1, 33));
+				r.val &= uint.MaxValue >> (32 - r.size);
 				bits.Add(r);
-				_writer.WriteBitsFromInt(r.Item1, r.Item2);
+				_writer.WriteBitsFromInt(r.val, r.size);
 			}
 			_reader = new BitStreamReader(_writer.AsArray);
-			for (int i = 0; i < Count; i++) {
+			for (int i = 0; i < Count; i++) 
 				Assert.AreEqual(bits[i].val, _reader.ReadBitsAsUInt(bits[i].size));
-			}
 			Assert.Pass();
 		}
 		
@@ -58,18 +56,17 @@ namespace Tests {
 		public void WriteSInts() {
 			List<(int val, int size)> bits = new List<(int val, int size)>();
 			
-			for (int i = 0; i < Count; i++)  {
-				var r = (_random.Next(), _random.Next(1, 33));
-				r.Item1 &= int.MaxValue >> (32 - r.Item2);
-				if ((r.Item1 & (1 << (r.Item2 - 1))) != 0) // sign extend, not necessary for primitive types
-					r.Item1 |= int.MaxValue << r.Item2;    // can use int here since the leftmost 0 will get shifted away anyway
+			for (int _ = 0; _ < Count; _++)  {
+				(int val, int size) r = (_random.Next(), _random.Next(1, 33));
+				r.val &= int.MaxValue >> (32 - r.size);
+				if ((r.val & (1 << (r.size - 1))) != 0) // sign extend, not necessary for primitive types
+					r.val |= int.MaxValue << r.size;    // can use int here since the leftmost 0 will get shifted away anyway
 				bits.Add(r);
-				_writer.WriteBitsFromInt(r.Item1, r.Item2);
+				_writer.WriteBitsFromInt(r.val, r.size);
 			}
 			_reader = new BitStreamReader(_writer.AsArray);
-			for (int i = 0; i < Count; i++) {
+			for (int i = 0; i < Count; i++) 
 				Assert.AreEqual(bits[i].val, _reader.ReadBitsAsSInt(bits[i].size));
-			}
 			Assert.Pass();
 		}
 
@@ -78,18 +75,40 @@ namespace Tests {
 		public void WriteUIntsIfExists() {
 			List<(uint? val, int size)> bits = new List<(uint? val, int size)>();
 			
-			for (int i = 0; i < Count; i++)  {
+			for (int _ = 0; _ < Count; _++)  {
 				bool rBool = _random.NextDouble() >= 0.5;
-				var r = (rBool ? (uint?)null : (uint)_random.Next(), _random.Next(1, 33));
-				r.Item1 &= uint.MaxValue >> (32 - r.Item2);
+				(uint? val, int size) r = (rBool ? (uint?)null : (uint)_random.Next(), _random.Next(1, 33));
+				r.val &= uint.MaxValue >> (32 - r.size);
 				bits.Add(r);
-				_writer.WriteBitsFromUIntIfExists(r.Item1, r.Item2);
+				_writer.WriteBitsFromUIntIfExists(r.val, r.size);
 			}
 			_reader = new BitStreamReader(_writer.AsArray);
-			for (int i = 0; i < Count; i++) {
+			for (int i = 0; i < Count; i++) 
 				Assert.AreEqual(bits[i].val, _reader.ReadBitsAsUIntIfExists(bits[i].size));
-			}
 			Assert.Pass();
+		}
+
+
+		[Test]
+		public void WriteBitCoord() {
+			for (int i = 0; i < Count; i++) {
+				int skip = i % 8;
+				BitStreamWriter bsw = new BitStreamWriter();
+				bsw.WriteBitsFromInt(_random.Next(), skip);
+				bsw.WriteBitCoord((float)((_random.NextDouble() - 0.5) * 100000));
+				BitStreamReader bsr = new BitStreamReader(bsw.AsArray);
+				bsr.SkipBits(skip);
+				// Write the float back and then read it again, this will constrain it to the precision of a 
+				// bit coord so we can do a direct equality check.
+				float f = bsr.ReadBitCoord();
+				// Now do all the same stuff we just did but with the constrained float.
+				bsw = new BitStreamWriter();
+				bsw.WriteBitsFromInt(_random.Next(), skip);
+				bsw.WriteBitCoord(f);
+				bsr = new BitStreamReader(bsw.AsArray);
+				bsr.SkipBits(skip);
+				Assert.AreEqual(bsr.ReadBitCoord(), f);
+			}
 		}
 	}
 }
