20cd21e74d97615fc33b8c0726c77b556d6a7a76
UncraftedName <uncraftedname@gmail.com>
2021-11-25 12:47:08 -0800

    simplify OptSmoothGlessHops.cs

--- ConsoleApp/src/DemoArgProcessing/Options/Hidden/OptSmoothGlessHops.cs
+++ ConsoleApp/src/DemoArgProcessing/Options/Hidden/OptSmoothGlessHops.cs
@@ -80,12 +80,10 @@ namespace ConsoleApp.DemoArgProcessing.Options.Hidden {
 		 * something like that. This makes jumps extremely ugly without demo interp and slightly bouncy with interp.
 		 * The goal of this function is to "bridge the gap" if you will, and pull the player's feet up during a jump.
 		 * We find jumps by looking at "m_Local.m_flJumpTime", and then linearly interpolate the player's origin and
-		 * view offset across the ticks when they are on the ground. There's some spicy stuff below, for instance I
-		 * re-encode the player's view origin, but I can only write it if my encoding takes the same amount of bits as
-		 * the original value. I'm sure there's plenty of memery that could cut out here, this is probably something
-		 * that should be improved in the future.
+		 * view offset across the ticks when they are on the ground. There's some spicy stuff below, this is probably
+		 * something that should be improved in the future.
 		 */
-		public static void SmoothJumps(SourceDemo demo, Stream s, int maxTicksBeforeJump) {
+		public static void SmoothJumps(SourceDemo demo, Stream s, int maxGroundTicks) {
 			
 			if ((demo.DemoInfo.DemoParseResult & DemoParseResult.EntParsingEnabled) == 0) {
 				Console.WriteLine("Entity parsing not enabled for this demo, can't apply smoothing.");
@@ -106,7 +104,6 @@ namespace ConsoleApp.DemoArgProcessing.Options.Hidden {
 			Console.WriteLine($"found jump ticks: {jumpTicks.SequenceToString()}");
 			BitStreamWriter bsw = new BitStreamWriter(demo.Reader.Data);
 
-			const int maxTicksBeyondJump = 1;
 			const float interpThreshold = 15; // this many units off the ground is considered a jump
 
 			var frames = demo.Frames;
@@ -120,102 +117,59 @@ namespace ConsoleApp.DemoArgProcessing.Options.Hidden {
 				Packet groundPacket = (Packet)frames[jumpTickIdx].Packet!;
 				Vector3 groundPos = groundPacket.PacketInfo[0].ViewOrigin; // roughly the ground pos
 
-				int endTick = -1;
-				Vector3 endVec = new Vector3(float.PositiveInfinity);
-				bool shouldInterp = true;
+				int endTick;
+				Vector3 endVec;
 
 				int idx = jumpTickIdx;
-				for (int ticksBeyondJump = 0; ticksBeyondJump < maxTicksBeyondJump; ticksBeyondJump++) {
+				while (frames[++idx].Type != PacketType.Packet) ; // jump to next packet
 
-					while (frames[++idx].Type != PacketType.Packet); // jump to next packet
+				Vector3 curViewOrigin = ((Packet)frames[idx].Packet!).PacketInfo[0].ViewOrigin;
+
+				if (curViewOrigin.Z - groundPos.Z > interpThreshold) {
+					endTick = ((Packet)frames[idx].Packet!).Tick;
+					endVec = curViewOrigin;
+				} else {
+					Console.WriteLine($"not patching jump on tick {jumpTick}, too many ticks before air time detected");
+					continue;
+				}
+				
+				Vector3 startVec = new Vector3(float.PositiveInfinity);
+				idx = jumpTickIdx;
+				bool shouldInterp = true;
+				
+				for (int ticksBeforeJump = 0; ticksBeforeJump < maxGroundTicks; ticksBeforeJump++) {
+
+					while (frames[--idx].Type != PacketType.Packet) ; // jump to previous packet
 
 					Packet curPacket = (Packet)frames[idx].Packet!;
-					Vector3 curViewOrigin = curPacket.PacketInfo[0].ViewOrigin;
+					curViewOrigin = curPacket.PacketInfo[0].ViewOrigin;
 
 					if (curViewOrigin.Z - groundPos.Z > interpThreshold) {
-						endTick = curPacket.Tick;
-						endVec = curViewOrigin;
+						startVec = curViewOrigin;
 						break;
 					}
-
-					if (ticksBeyondJump == maxTicksBeyondJump - 1) {
+					if (ticksBeforeJump == maxGroundTicks - 1) {
 						shouldInterp = false;
-						Console.WriteLine($"not patching jump on tick {jumpTick}, too many ticks before air time detected");
+						Console.WriteLine($"not patching jump on tick {jumpTick}, too many ticks on ground (at least {maxGroundTicks}) before this jump");
 						break;
 					}
 				}
 				if (!shouldInterp)
 					continue;
 				
-				Vector3 startVec = new Vector3(float.PositiveInfinity);
-
-				idx = jumpTickIdx;
-				for (int ticksBeforeJump = 0; ticksBeforeJump < maxTicksBeforeJump; ticksBeforeJump++) {
-
-					while (frames[--idx].Type != PacketType.Packet) ; // jump to previous packet
-
-					Packet curPacket = (Packet)frames[idx].Packet!;
-					Vector3 curViewOrigin = curPacket.PacketInfo[0].ViewOrigin;
-
-					if (curViewOrigin.Z - groundPos.Z > interpThreshold) {
-						startVec = curViewOrigin;
-						break;
-					}
-					if (ticksBeforeJump == maxTicksBeforeJump - 1) {
-						shouldInterp = false;
-						Console.WriteLine($"not patching jump on tick {jumpTick}, too many ticks on ground (at least {maxTicksBeforeJump}) before this jump");
-						break;
-					}
-				}
-				if (!shouldInterp)
-					continue;
 				int startTick = frames[idx].Tick; // idx is currently the start tick (right before the first interp tick)
 
 				while (frames[idx].Tick < endTick) {
 
 					while (frames[++idx].Type != PacketType.Packet); // jump to next packet
 
-					BitStreamWriter simpleVecBytes = new BitStreamWriter();
+					BitStreamWriter vecBytes = new BitStreamWriter();
 					Packet packet = (Packet)frames[idx].Packet!;
 					float lerpAmount = (float)(frames[idx].Tick - startTick) / (endTick - startTick);
 					Vector3 lerpVec = Vector3.Lerp(startVec, endVec, lerpAmount);
-					simpleVecBytes.WriteVector3(lerpVec);
+					vecBytes.WriteVector3(lerpVec);
 					// edit the pos in the cmdinfo
-					bsw.EditBitsAtIndex(simpleVecBytes, packet.Reader.AbsoluteStart + 32);
-					// edit the pos in the entity delta
-					var originProp = (SingleEntProp<Vector3>?)(
-							(Delta?)packet.FilterForMessage<SvcPacketEntities>().Single().Updates!
-								.SingleOrDefault(update => update.ServerClass.ClassName == "CPortal_Player"))
-						?.Props.SingleOrDefault(tuple => tuple.prop.Name == "m_vecOrigin").prop;
-					if (originProp != null) {
-						bool tryWrite = true;
-						while (tryWrite) {
-							tryWrite = false;
-							BitStreamWriter newOriginBytes = new BitStreamWriter();
-							newOriginBytes.WriteBitCoord(originProp.Value.X);
-							newOriginBytes.WriteBitCoord(originProp.Value.Y);
-							newOriginBytes.WriteBitCoord(lerpVec.Z);
-							// only edit the pos if the new value is encoded with the same number of bits
-							switch (newOriginBytes.BitLength - originProp.BitLength) {
-								case 0:
-									bsw.EditBitsAtIndex(newOriginBytes, originProp.Offset, originProp.BitLength);
-									break;
-								case 5: // too many bits, but we can shave some off by rounding the z val
-									lerpVec.Z = (float)Math.Round(lerpVec.Z);
-									tryWrite = true;
-									break;
-								case -5: // too few bits, but we can increase the encoding size by giving z a fractional part
-									lerpVec.Z += 0.04f;
-									tryWrite = true;
-									break;
-								default:
-									Console.WriteLine(
-										$"not patching jump on tick {jumpTick}, " +
-										$"bit length doesn't match: new value uses {newOriginBytes.BitLength} bits but old value has {originProp.BitLength} bits");
-									break;
-							}
-						}
-					}
+					bsw.EditBitsAtIndex(vecBytes, packet.Reader.AbsoluteStart + 32);
 
 					var viewOffsetProp = (SingleEntProp<float>?)(
 							(Delta?)packet.FilterForMessage<SvcPacketEntities>().Single().Updates!
