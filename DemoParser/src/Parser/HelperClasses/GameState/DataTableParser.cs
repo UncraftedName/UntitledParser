68ffa804d4c5873661b3fae155bb637e772b0f08
UncraftedName <uncraftedname@gmail.com>
2022-01-29 01:35:14 -0800

    simplify DataTableParser.cs

--- DemoParser/src/Parser/HelperClasses/GameState/DataTableParser.cs
+++ DemoParser/src/Parser/HelperClasses/GameState/DataTableParser.cs
@@ -1,7 +1,6 @@
 #nullable enable
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Diagnostics;
 using System.Linq;
 using DemoParser.Parser.Components.Messages;
 using DemoParser.Parser.Components.Packets;
@@ -19,77 +18,28 @@ namespace DemoParser.Parser.HelperClasses.GameState {
 	public class DataTableParser {
 
 		private readonly SourceDemo _demoRef;
-		private DemoInfo DemSet => _demoRef.DemoInfo;
 		private readonly DataTables _dtRef;
-		private readonly ImmutableDictionary<string, SendTable> _tableLookup;
 		public int ServerClassBits => BitUtils.HighestBitIndex((uint)_dtRef.ServerClasses.Count) + 1; // this might be off for powers of 2
-		public readonly PropLookup? FlattenedProps; // not initialized during the server class info
+		public PropLookup? FlattenedProps {get;private set;} // not initialized during the server class info
 
 
 
 		public DataTableParser(SourceDemo demoRef, DataTables dtRef) {
 			_demoRef = demoRef;
 			_dtRef = dtRef;
-			FlattenedProps = new PropLookup();
-			// fill up the lookup table so we can quickly reference the tables by name later
-			_tableLookup = dtRef.Tables.ToImmutableDictionary(table => table.Name, table => table);
 		}
 
 
-		// if allow modify is not set then we don't touch the actual demo state
+		// if allow modify is set then we'll parse the baselines in the prior string tables
 		public void FlattenClasses(bool allowModifyDemo) {
-			for (int classIndex = 0; classIndex < _dtRef.ServerClasses.Count; classIndex++) {
-				ServerClass currentClass = _dtRef.ServerClasses[classIndex];
+			var tableLookup = _dtRef.Tables.ToImmutableDictionary(table => table.Name, table => table);
 
-				HashSet<(string, string)> excludes = new HashSet<(string tableName, string propName)>();
-				List<ServerClass> baseClasses = new List<ServerClass>();
-				SendTable table = _dtRef.Tables[currentClass.DataTableId];
+			FlattenedProps = new PropLookup(_dtRef.ServerClasses.Count);
 
-				GatherExcludesAndBaseClasses(excludes, baseClasses, table, true);
-				GatherProps(excludes, table, classIndex, "");
-
-				List<FlattenedProp> fProps = FlattenedProps[currentClass.DataTableId].flattenedProps;
-
-				// Now we have the props, rearrange them so that props that are marked with 'changes often' get a
-				// smaller index. In the new protocol the priority of the props is also taken into account.
-				if (_demoRef.DemoInfo.NewDemoProtocol && !_demoRef.DemoInfo.IsLeft4Dead1()) {
-					int start = 0;
-					var priorities = fProps.Select(entry => entry.PropInfo.Priority!.Value).Concat(new[] {64}).Distinct();
-					foreach (int priority in priorities.OrderBy(i => i)) {
-						while (true) {
-							int currentProp = start;
-							while (currentProp < fProps.Count) {
-								SendTableProp prop = fProps[currentProp].PropInfo;
-								// ChangesOften gets the same priority as 64
-								if (prop.Priority == priority || (DemSet.PropFlagChecker.HasFlag(prop.Flags, ChangesOften) && priority == 64)) {
-									if (start != currentProp)
-										(fProps[start], fProps[currentProp]) = (fProps[currentProp], fProps[start]);
-									start++;
-									break;
-								}
-								currentProp++;
-							}
-							if (currentProp == fProps.Count)
-								break;
-						}
-					}
-				} else { // se2007/engine/dt.cpp line 443
-					int start = 0;
-					while (true) {
-						int i;
-						for (i = start; i < fProps.Count; i++) {
-							FlattenedProp p = fProps[i];
-							if (DemSet.PropFlagChecker.HasFlag(p.PropInfo.Flags, ChangesOften)) {
-								fProps[i] = fProps[start];
-								fProps[start] = p;
-								start++;
-								break;
-							}
-						}
-						if (i == fProps.Count)
-							break;
-					}
-				}
+			foreach (ServerClass serverClass in _dtRef.ServerClasses) {
+				SendTable table = _dtRef.Tables[serverClass.DataTableId];
+				GatherProps(tableLookup, GatherExcludes(tableLookup, table), table, serverClass, "");
+				SortProps(FlattenedProps[serverClass.DataTableId].flattenedProps);
 			}
 
 			// Now that I know the order of the props, I will parse the baselines of any SvcCreateMessages that
@@ -108,73 +58,103 @@ namespace DemoParser.Parser.HelperClasses.GameState {
 		}
 
 
-		private void GatherExcludesAndBaseClasses(
-			ISet<(string, string)> excludes,
-			ICollection<ServerClass> baseClasses,
-			SendTable table,
-			bool collectBaseClasses)
-		{
-			excludes.UnionWith(
-				table.SendProps
-					.Where(stp => DemSet.PropFlagChecker.HasFlag(stp.Flags, Exclude))
-					.Select(stp => (stp.ExcludeDtName!, stp.Name))
-				);
-
-			foreach (SendTableProp property in table.SendProps.Where(property => property.SendPropType == SendPropType.DataTable)) {
-				if (collectBaseClasses && property.Name == "baseclass") {
-					GatherExcludesAndBaseClasses(excludes, baseClasses, _tableLookup[property.ExcludeDtName!], true);
-					baseClasses.Add(GetClassByDtName(table.Name)); // should be the same as the properties table
-				} else {
-					GatherExcludesAndBaseClasses(excludes, baseClasses, _tableLookup[property.ExcludeDtName!], false);
-				}
+		private ISet<(string propName, string tableName)> GatherExcludes(IReadOnlyDictionary<string, SendTable> tableLookup, SendTable table) {
+			var excludes = new HashSet<(string, string)>();
+			foreach (SendTableProp sendProp in table.SendProps) {
+				if (sendProp.SendPropType == SendPropType.DataTable)
+					excludes.UnionWith(GatherExcludes(tableLookup, tableLookup[sendProp.ExcludeDtName!]));
+				else if (_demoRef.DemoInfo.PropFlagChecker.HasFlag(sendProp.Flags, Exclude))
+					excludes.Add((sendProp.ExcludeDtName!, sendProp.Name));
 			}
+			return excludes;
 		}
 
 
-		private void GatherProps(HashSet<(string, string)> excludes, SendTable table, int classIndex, string prefix) {
-			List<FlattenedProp> tmpFlattedProps = new List<FlattenedProp>();
-			IterateProps(excludes, table, classIndex, tmpFlattedProps, prefix);
-
-			ServerClass classRef = _dtRef.ServerClasses[classIndex];
-			int tmpIndex = classRef.DataTableId;
-			// in theory the class ID's should always be in consecutive order, although demos seem to imply otherwise
-			Debug.Assert(tmpIndex <= FlattenedProps.Count, "server class names are not in consecutive order");
-
-			if (tmpIndex == FlattenedProps.Count) // never greater, assert checks for that
-				FlattenedProps.Add((classRef, new List<FlattenedProp>()));
-
-			FlattenedProps[tmpIndex].flattenedProps.AddRange(tmpFlattedProps);
+		private void GatherProps(
+			IReadOnlyDictionary<string, SendTable> tableLookup,
+			ISet<(string, string)> excludes,
+			SendTable table,
+			ServerClass serverClass,
+			string prefix)
+		{
+			List<FlattenedProp> fProps = new List<FlattenedProp>(table.SendProps.Count);
+			IterateProps(tableLookup, excludes, table, serverClass, fProps, prefix);
+			if (serverClass.DataTableId == FlattenedProps.Count)
+				FlattenedProps.Add((serverClass, new List<FlattenedProp>()));
+			FlattenedProps[serverClass.DataTableId].flattenedProps.AddRange(fProps);
 		}
 
 
 		private void IterateProps(
-			HashSet<(string, string)> excludes,
+			IReadOnlyDictionary<string, SendTable> tableLookup,
+			ISet<(string, string)> excludes,
 			SendTable table,
-			int classIndex,
+			ServerClass serverClass,
 			ICollection<FlattenedProp> fProps,
 			string prefix)
 		{
 			for (int i = 0; i < table.SendProps.Count; i++) {
 				SendTableProp prop = table.SendProps[i];
-				if (DemSet.PropFlagChecker.HasFlags(prop.Flags, Exclude, InsideArray) || excludes.Contains((table.Name, prop.Name)))
+				if (_demoRef.DemoInfo.PropFlagChecker.HasFlags(prop.Flags, Exclude, InsideArray) || excludes.Contains((table.Name, prop.Name)))
 					continue;
 				if (prop.SendPropType == SendPropType.DataTable) {
-					SendTable subTable = _tableLookup[prop.ExcludeDtName!];
+					SendTable subTable = tableLookup[prop.ExcludeDtName!];
 					// we don't prefix Collapsible stuff, since it is just derived mostly
-					if (DemSet.PropFlagChecker.HasFlag(prop.Flags, Collapsible))
-						IterateProps(excludes, subTable, classIndex, fProps, prefix);
+					if (_demoRef.DemoInfo.PropFlagChecker.HasFlag(prop.Flags, Collapsible))
+						IterateProps(tableLookup, excludes, subTable, serverClass, fProps, prefix);
 					else
-						GatherProps(excludes, subTable, classIndex, prop.Name.Length > 0 ? $"{prop.Name}." : "");
+						GatherProps(tableLookup, excludes, subTable, serverClass, prop.Name.Length > 0 ? prop.Name + "." : "");
 				} else {
-					fProps.Add(new FlattenedProp(DemSet, prefix + prop.Name, prop,
+					fProps.Add(new FlattenedProp(_demoRef.DemoInfo, prefix + prop.Name, prop,
 						prop.SendPropType == SendPropType.Array ? table.SendProps[i - 1] : null));
 				}
 			}
 		}
 
 
-		public ServerClass GetClassByDtName(string dtName) {
-			return _dtRef.ServerClasses!.Single(serverClass => serverClass.DataTableName == dtName);
+		// Properties that have a smaller index take up less bits in a delta, so some that are frequently updated get a
+		// priority or a "ChangesOften" flag in newer versions which bumps them up in the table. This is done with a
+		// custom non-stable sort.
+		private void SortProps(IList<FlattenedProp> fProps) {
+			if (_demoRef.DemoInfo.NewDemoProtocol && !_demoRef.DemoInfo.IsLeft4Dead1()) {
+				int start = 0;
+				var priorities = fProps.Select(entry => entry.PropInfo.Priority!.Value).Concat(new[] { 64 }).Distinct();
+				foreach (int priority in priorities.OrderBy(i => i)) {
+					while (true) {
+						int i = start;
+						while (i < fProps.Count) {
+							SendTableProp p = fProps[i].PropInfo;
+							// ChangesOften gets the same priority as 64
+							if (p.Priority == priority || (_demoRef.DemoInfo.PropFlagChecker.HasFlag(p.Flags, ChangesOften) && priority == 64)) {
+								if (start != i)
+									(fProps[start], fProps[i]) = (fProps[i], fProps[start]);
+								start++;
+								break;
+							}
+							i++;
+						}
+						if (i == fProps.Count)
+							break;
+					}
+				}
+			}
+			else { // se2007/engine/dt.cpp line 443
+				int start = 0;
+				while (true) {
+					int i;
+					for (i = start; i < fProps.Count; i++) {
+						FlattenedProp p = fProps[i];
+						if (_demoRef.DemoInfo.PropFlagChecker.HasFlag(p.PropInfo.Flags, ChangesOften)) {
+							fProps[i] = fProps[start];
+							fProps[start] = p;
+							start++;
+							break;
+						}
+					}
+					if (i == fProps.Count)
+						break;
+				}
+			}
 		}
 	}
 
@@ -217,5 +197,7 @@ namespace DemoParser.Parser.HelperClasses.GameState {
 
 
 	// if only we had typedefs...
-	public class PropLookup : List<(ServerClass serverClass, List<FlattenedProp> flattenedProps)> {}
+	public class PropLookup: List<(ServerClass serverClass, List<FlattenedProp> flattenedProps)> {
+		public PropLookup(int capacity) : base(capacity) {}
+	}
 }
