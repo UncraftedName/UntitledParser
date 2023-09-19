0de593ec82e7526b0df8fcfe971dbbe713e3b677
Willian Henrique <wsimanbrazil@yahoo.com.br>
2023-09-19 14:28:30 -0300

    Add unknown L4D-specific field in SvcVoiceData

--- DemoParser/src/Parser/Components/Messages/SvcVoiceData.cs
+++ DemoParser/src/Parser/Components/Messages/SvcVoiceData.cs
@@ -1,6 +1,7 @@
 using DemoParser.Parser.Components.Abstract;
 using DemoParser.Utils;
 using DemoParser.Utils.BitStreams;
+using System;
 
 namespace DemoParser.Parser.Components.Messages;
 
@@ -9,6 +10,7 @@ public class SvcVoiceData : DemoMessage {
 	public byte FromClient;
 	public bool Proximity;
 	public int BitLen;
+	public bool[]? Unknown;
 
 	public SvcVoiceData(SourceDemo? demoRef, byte value) : base(demoRef, value) {}
 
@@ -17,7 +19,11 @@ public class SvcVoiceData : DemoMessage {
 		FromClient = bsr.ReadByte();
 		Proximity = bsr.ReadByte() != 0;
 		BitLen = bsr.ReadUShort();
-
+		if (DemoRef.DemoInfo.IsLeft4Dead()) {
+			Unknown = new bool[4];
+			for (int i = 0; i < 4; i++)
+				Unknown[i] = bsr.ReadBool();
+		}
 		BitStreamReader dataBsr = bsr.ForkAndSkip(BitLen);
 	}
 
@@ -25,5 +31,12 @@ public class SvcVoiceData : DemoMessage {
 		pw.AppendLine($"client: {FromClient}");
 		pw.AppendLine($"proximity: {Proximity}");
 		pw.Append($"bit length: {BitLen}");
+		if (Unknown != null ) {
+			pw.AppendLine();
+			pw.Append("unknown bits: ");
+			foreach (var v in Unknown) {
+				pw.Append($" {v}");
+			}
+		}
 	}
 }
