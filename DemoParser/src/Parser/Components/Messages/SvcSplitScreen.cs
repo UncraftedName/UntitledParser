05e84e968acc67db10512391caaf7c31bfc499e0
UncraftedName <uncraftedname@gmail.com>
2026-02-09 16:59:47 -0800

    Fix incorrect behavior in SvcSplitScreen

--- DemoParser/src/Parser/Components/Messages/SvcSplitScreen.cs
+++ DemoParser/src/Parser/Components/Messages/SvcSplitScreen.cs
@@ -4,11 +4,12 @@ using DemoParser.Utils.BitStreams;
 
 namespace DemoParser.Parser.Components.Messages {
 
+	// format is grabbed from p2 dissasembly + strike protobuf messages
 	public class SvcSplitScreen : DemoMessage {
 
 		public bool RemoveUser;
-		private BitStreamReader _data;
-		public BitStreamReader Data => _data.FromBeginning(); // todo
+		public int Slot;
+		public int PlayerIndex;
 
 
 		public SvcSplitScreen(SourceDemo? demoRef, byte value) : base(demoRef, value) {}
@@ -16,14 +17,15 @@ namespace DemoParser.Parser.Components.Messages {
 
 		protected override void Parse(ref BitStreamReader bsr) {
 			RemoveUser = bsr.ReadBool();
-			uint dataLen = bsr.ReadUInt(11);
-			_data = bsr.ForkAndSkip((int)dataLen);
+			Slot = bsr.ReadBool() ? 1 : 0;
+			PlayerIndex = bsr.ReadSInt(DemoInfo.MaxEdictBits);
 		}
 
 
 		public override void PrettyWrite(IPrettyWriter pw) {
 			pw.AppendLine(RemoveUser ? "remove user" : "add user");
-			pw.Append($"data of length {Data.BitLength}");
+			pw.AppendLine($"slot: {Slot}");
+			pw.Append($"player index: {PlayerIndex}");
 		}
 	}
 }
